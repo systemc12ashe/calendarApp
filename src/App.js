20acd1bf0ca3aebef4212c361f3e2396bdcdad53
An Tran <aht49@drexel.edu>
2022-03-12 18:26:39 -0500

    added private routing

--- src/App.js
+++ src/App.js
@@ -3,19 +3,30 @@ import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 import Calendar from "./components/Calendar";
 import "./App.css";
+import { AuthProvider } from "./contexts/AuthContext";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
     <>
     {}
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/SignUp" element={<SignUp />} />
-        <Route path="/Calendar" element={<Calendar />} />
-      </Routes>
+    
+    <BrowserRouter> 
+      <AuthProvider>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/SignUp" element={<SignUp />} />
+          <Route
+            path="/Calendar" 
+            element={<PrivateRoute>
+              <Calendar />
+            </PrivateRoute>}
+          />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
+    
     </>
   );
 }
